ad46a9768b9bf00377075c2113f4ae07e7b748f7
Anjo Weichbrodt <anjo.weichbrodt@gmail.com>
2021-05-21 10:19:40 +0200

    Update install_script_arch.sh

--- install_script_arch.sh
+++ install_script_arch.sh
@@ -15,7 +15,7 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub wayland qt5-wayland glfw-wayland pipewire efibootmgr networkmanager dialog wpa_supplicant dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip openssh rsync reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g
+pacman -S grub wayland qt5-wayland glfw-wayland pipewire efibootmgr networkmanager flatpak dialog wpa_supplicant dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip openssh rsync reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings

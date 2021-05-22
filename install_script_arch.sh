8ffe57d4dbdd41ab8948ac9420f8103fdc001163
Anjo Weichbrodt <anjo.weichbrodt@gmail.com>
2021-05-22 07:16:19 +0200

    Update install_script_arch.sh

--- install_script_arch.sh
+++ install_script_arch.sh
@@ -12,13 +12,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
-
 pacman -S grub wayland qt5-wayland glfw-wayland pipewire efibootmgr networkmanager flatpak dialog wpa_supplicant reflector dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups openssh rsync reflector acpi acpi_call tlp dnsmasq ipset firewalld sof-firmware nss-mdns acpid
-
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -27,7 +21,7 @@ systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable tlp
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable firewalld

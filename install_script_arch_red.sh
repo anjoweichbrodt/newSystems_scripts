87255bfbc5c7d33b0595582ba23a7a7e6ec057c6
Anjo Weichbrodt <anjo.weichbrodt@gmail.com>
2021-05-23 06:54:57 +0200

    Update install_script_arch_red.sh

--- install_script_arch_red.sh
+++ install_script_arch_red.sh
@@ -5,22 +5,24 @@ hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=us-acentos" >> /etc/vconsole.conf
+echo "KEYMAP=us" >> /etc/vconsole.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S grub wayland qt5-wayland glfw-wayland pipewire efibootmgr networkmanager base-devel bluez bluez-utils cups openssh rsync tlp
+pacman -S grub wayland qt5-wayland glfw-wayland pipewire efibootmgr networkmanager base-devel bluez bluez-utils cups openssh rsync tlp firewalld nftables acpid
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable tlp
+systemctl enable firewalld
+systemctl enable acpid
 
 useradd -m anjo
 echo anjo:password | chpasswd

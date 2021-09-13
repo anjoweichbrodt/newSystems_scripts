95a1f5f9b6b11b55464386c9a921a97a3215ca29
Anjo Weichbrodt <anjo.weichbrodt@gmail.com>
2021-09-13 04:37:17 +0200

    Update dotfile_setup.sh
    Include alias for fish

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -3,6 +3,7 @@
 git clone --bare git://github.com/anjoweichbrodt/dotfiles.git $HOME/.dotfiles
 
 echo "alias dotconf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
+echo "alias dotconf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.config/fish/config.fish
 
 function dotconf {
         /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@

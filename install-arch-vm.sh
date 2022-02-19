8e07de54d6806d3e4cb5ee7d4b4c5abab19add61
Anjo Weichbrodt <anjo.weichbrodt@gmail.com>
2022-02-19 18:47:05 +0100

    Create install-arch-vm.sh

---
+++ install-arch-vm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call qemu-guest-agent edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g inconsolata-font
+
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings xorg
+
+grub-install --target=i386-pc /dev/sdX # replace sdx with your disk name, not the partition
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable firewalld
+systemctl enable acpid
+systemctl enable qemu-guest-agent
+
+useradd -m anjo
+echo anjo:password | chpasswd
+
+echo "anjo ALL=(ALL) ALL" >> /etc/sudoers.d/anjo
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+

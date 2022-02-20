3de82be3953de830b8db90fd7d2cf32d44b31b65
Anjo Weichbrodt <anjo.weichbrodt@gmail.com>
2022-02-20 17:57:00 +0100

    Update install-arch-vm.sh

--- install-arch-vm.sh
+++ install-arch-vm.sh
@@ -14,7 +14,7 @@ echo root:password | chpasswd
 
 pacman -S grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call qemu-guest-agent edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g ttf-inconsolata
 
-grub-install --target=i386-pc /dev/vda1
+grub-install --target=i386-pc /dev/vda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
